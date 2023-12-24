d6e575c50b70f55697abcbab46911fb90efaf46f
Andreas Erdes <socialcoms@posteo.de>
2023-12-24 21:07:43 +0100

    Setup Rust flake for NixOS

--- flake.nix
+++ flake.nix
@@ -24,6 +24,10 @@
           shellHook = ''
             export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
             export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+            export BINS='.flake-bins'
+
+            mkdir -p "$BINS"
+            ln -sfn ${pkgs.rustup} "$BINS/rustup"
           '';
           # Add precompiled library to rustc search path
           RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
